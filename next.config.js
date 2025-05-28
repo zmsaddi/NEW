988d15eb68af238c5b8e76986d35485c52b472d2
zmsaddi <35869249+zmsaddi@users.noreply.github.com>
2025-05-28 07:19:42 +0000

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+    localeDetection: true,
+  },
+};
+
+module.exports = nextConfig;
